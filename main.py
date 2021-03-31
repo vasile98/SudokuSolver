54a27c8f743f64d4dd9538e6d62ff8358af9dcd7
Vasile Antohi <vantohi1@sheffield.ac.uk>
2021-03-31 17:43:25 +0100

    Added a sample grid for testing and setup the todolist

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+# @todo function to find empty locations
+# @todo functions to find if a number is used in a row column or box
+# @todo implement the main solver function 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# Main functions to test
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    # create a 9x9 grid for testing
+    grid = [[3, 0, 6, 5, 0, 8, 4, 0, 0],
+            [5, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 8, 7, 0, 0, 0, 0, 3, 1],
+            [0, 0, 3, 0, 1, 0, 0, 8, 0],
+            [9, 0, 0, 8, 6, 3, 0, 0, 5],
+            [0, 5, 0, 0, 9, 0, 6, 0, 0],
+            [1, 3, 0, 0, 0, 0, 2, 5, 0],
+            [0, 0, 0, 0, 0, 0, 0, 7, 4],
+            [0, 0, 5, 2, 0, 6, 3, 0, 0]]
